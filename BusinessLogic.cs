d1fefc6d3806abafedb4d606a0941e54b590e314
zannlin <zanderlindeque1@gmail.com>
2024-11-04 10:25:47 +0200

    Added Report functionality

--- BusinessLogic.cs
+++ BusinessLogic.cs
@@ -8,5 +8,28 @@ namespace StartSmartStudentManagement
 {
     internal class BusinessLogic
     {
+
+        public List<string> GenerateReport(string path, string reportPath)
+        {
+            List<string> students = FileHandler.Read(path);
+            int numStudents = 0;
+            int totalAge = 0;
+
+            foreach (string student in students)
+            {
+                string[] details = student.Split(',');
+                numStudents++;
+                totalAge += int.Parse(details[2]);
+            }
+
+            List<string> results = new List<string>();
+            results.Add($"Total students: {numStudents}");
+            results.Add($"Average age: {totalAge / numStudents}");
+            FileHandler.Overwrite(reportPath, results);
+
+
+            return results;
+        }
+
     }
 }
